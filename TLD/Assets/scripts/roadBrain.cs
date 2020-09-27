5c7b045794199f059ed09e31f1429b9184175f22
Denis <aripash@gmail.com>
2020-09-27 04:30:26 +0300

    started with the traffic light

--- TLD/Assets/scripts/roadBrain.cs
+++ TLD/Assets/scripts/roadBrain.cs
@@ -14,6 +14,7 @@ public class roadBrain : MonoBehaviour
     float secondNumber = 0;
     List<Vector3> list;
     int mid = 0;
+    [SerializeField] GameObject trafficLight = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,15 +42,28 @@ public class roadBrain : MonoBehaviour
     /// <summary>
     /// when you add a road, it needs to update the constraints when it touches another road
     /// activates automaticly
+    /// adds a "traffic light" between them and adds to the list of points
     /// </summary>
     /// <param name="collision"></param>
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "road") { 
-        constraints.addcons(gameObject.name + "," + collision.gameObject.name);
+            constraints.addcons(gameObject.name + "," + collision.gameObject.name);
+
+            //place of the new traffic light
+            Vector3 TL = collision.GetContact(0).point + collision.GetContact(1).point;
+            TL /= 2;
+            TL += collision.GetContact(3).point;
+            TL /= 2;
+
+            //create the traffic light
+            Instantiate(trafficLight, TL, trafficLight.transform.rotation);
+
+            //add the traffic light to the list
+
+        }
     }
         
-    }
     /// <summary>
     /// creates cars on the road, the cars will drive from start to end and through middle if it exists
     /// </summary>
