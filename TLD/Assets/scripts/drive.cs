8893790cc9d9a40b8f5c2e307b66b39c7fb0beb6
Denis <aripash@gmail.com>
2020-10-08 12:39:01 +0300

    car fixes
    increased car mass to metigate floating when crashing and fixed lookAt to not allways look at the target each frame, rather change the rotation when changing point and fixed colliders

--- TLD/Assets/scripts/drive.cs
+++ TLD/Assets/scripts/drive.cs
@@ -11,42 +11,44 @@ public class drive : MonoBehaviour
     public int maxpoints = 2;
     Rigidbody rb;
     [SerializeField]float safeDistance = 1;
+    private static int serial=0;
+
 
     // Update is called once per frame
     void Update()
     {
         if (currentpoint > 0)
         {
-               //avoid hitting cars and red trafficlights
-               RaycastHit hit;
-               Vector3 raycastpos = transform.position;
-               raycastpos.y += 0.25f;
-               Vector3 dir = (gps[currentpoint] - gps[currentpoint - 1]).normalized;
-               //cast a ray from raycastpos in the direction of movement at a safeDistance distance only on objects from the 8th layer and save to hit
-               if (Physics.Raycast(raycastpos, dir, out hit, safeDistance))
-               {
-                   if (hit.collider.tag == "car") { rb.velocity = Vector3.zero; }             
-               }
-                else
-                {
-                    rb.velocity = dir * speed;
-                }
-            //allways look at the next point
-            transform.LookAt(gps[currentpoint]);
+            //avoid hitting cars and red trafficlights
+            RaycastHit hit;
+            Vector3 raycastpos = gameObject.transform.position;
+            Vector3 dir = (gps[currentpoint] - gps[currentpoint - 1]).normalized;
+            //cast a ray from raycastpos in the direction of movement at a safeDistance distance only on objects from the 8th layer and save to hit
+            if (Physics.Raycast(raycastpos, dir, out hit, safeDistance))
+            {
+                if (hit.collider.tag == "car") { rb.velocity = Vector3.zero; }
+            }
+            else
+            {
+                rb.velocity = dir * speed;
+                transform.LookAt(gps[currentpoint]);
+            }
 
             Vector3 currentGPS = gameObject.transform.position;
             //check if we got to the next point
             if (Mathf.Abs(gps[currentpoint].x - currentGPS.x) < 0.1f && Mathf.Abs(gps[currentpoint].z - currentGPS.z) < 0.1f)
             {
+                gameObject.transform.position = gps[currentpoint];
                 currentpoint++;
                 if (currentpoint >= maxpoints) Destroy(gameObject);
                 else
                 {
                     dir = (gps[currentpoint] - gps[currentpoint - 1]).normalized;
                     rb.velocity = dir * speed;
+                    transform.LookAt(gps[currentpoint]);
                 }
             }
-            
+
         }
     }
     public void firstSpeed() {
@@ -54,5 +56,11 @@ public class drive : MonoBehaviour
         Vector3 dir = (gps[1] - gps[0]).normalized;
         rb.velocity = dir * speed;
         transform.LookAt(gps[1]);
+        gameObject.name ="car no."+ serial++;
     }
+/*
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("car crash "+name+"  "+collision.gameObject.name);
+    }*/
 }
